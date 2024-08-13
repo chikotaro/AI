30e2b50eaa19cab387a013db3a2bdb81a2a36654
chikotaro <140727421+chikotaro@users.noreply.github.com>
2024-08-13 09:24:16 +0900

    🔥 カードの記述を削除

--- js/script.js
+++ js/script.js
@@ -45,42 +45,6 @@ document.addEventListener("DOMContentLoaded", checkWidthAndRemoveClass);
 
 window.addEventListener("resize", checkWidthAndRemoveClass);
 
-// pick up ----
-const swiperWrapper = document.querySelector(".swiper-wrapper");
-const pickUpList = [
-	{
-		img: "pickup-image1.png",
-		title: "株式会社〇〇〇〇",
-		description:
-			"TaskMentorAIを導入して以来、業務の合理化とスキル向上が進み、社員たちはより満足度の高い仕事ができています。特にAIエージェントのサポートは頼りになります。",
-	},
-	{
-		img: "pickup-image2.png",
-		title: "〇〇大学〇〇研究室",
-		description:
-			"TaskMentorAIの導入により、教職員がAIエージェントを利用して、教育研究業務の効率向上しました。また、学内のAIリテラシーも向上しました。",
-	},
-	{
-		img: "pickup-image1.png",
-		title: "株式会社〇〇〇〇",
-		description:
-			"情報の一元管理とAIエージェントの質の高さに驚いています。ビジネスの未来に向けて頼りになるパートナーです。",
-	},
-	{
-		img: "pickup-image2.png",
-		title: "株式会社〇〇〇〇",
-		description:
-			"WA2の導入により、当社の技術者たちは最先端の知識を手に入れ、プロジェクトの成功に向けてよりスムーズに作業できるようになりました。当社に必須のツールです。",
-	},
-];
-
-for (let i = 0; i < pickUpList.length; i++) {
-	const { img, title, description } = pickUpList[i];
-	const content = `<div class="swiper-slide pickUp-cards"><div class="pickUp-card"><img src="./images/${img}" alt="" /><div class="pickUp-card-ttl">${title}</div><p class="pickUp-card-desc">${description}</p></div></div>`;
-
-	swiperWrapper.insertAdjacentHTML("afterbegin", content);
-}
-
 // swiper----
 
 const swiper = new Swiper(".swiper", {
